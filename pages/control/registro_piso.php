2df2ae064920025ae4d55605892e88f230400a84
rioyi <rioyi_kaji@hotmail.co.jp>
2016-01-26 16:44:36 -0430

    Se agrego el fomulario de piso - se creo registro de piso - se registra los piso

---
+++ pages/control/registro_piso.php
@@ -0,0 +1,33 @@
+<?php
+$piso = $_POST['piso'];
+$descripcion = $_POST['descripcion'];
+
+
+
+
+
+
+#conexion la base de datos
+require("../control/conexion_bd.php");
+
+#consulta para corroborar no dublicidad de ci_escolar
+
+			#REGISTRO DOCENTE
+			mysql_query("INSERT INTO piso (descripcion_piso, numero_piso) VALUES ('$descripcion','$piso')") or die (mysql_error());
+			
+			
+			
+			mysql_close($link);
+			#header('location: emergente/registro_alumno.php');
+			echo "Se a Registrado el piso";
+		
+
+
+
+
+
+
+
+
+
+?>
