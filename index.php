4ff47d561492f0b99d99eeeaf66f6d874a54bbfa
rioyi <rioyi_kaji@hotmail.co.jp>
2016-02-22 16:22:33 -0430

    Se agregro menu y menu_form para tener el menu en un archivo separado la vista, se agrego el archivo de verificacion para comenzar el registro mensual, se agreo al menu la nueva opcion de registro mensual

--- index.php
+++ index.php
@@ -105,30 +105,10 @@
                         </div>
                     </form>
                     <!-- /.formulario del buscado -->
-                    <!-- sidebar del menu: el estilo puedeo ser encontrado en sidebar.less -->
-                    <ul class="sidebar-menu">
-                        <li class="active">
-                            <a href="index.html">
-                                <i class="fa fa-dashboard"></i> <span>Menu Principal</span>
-                            </a>
-                        </li>
-
-
-
-                        <li class="treeview">
-                            <a href="#">
-                                <i class="fa fa-th"></i>
-                                <span>Misceláneo</span>
-                                <i class="fa fa-angle-left pull-right"></i>
-                            </a>
-                            <ul class="treeview-menu">
-                                <li><a href="pages/forms/form_periodo.php"><i class="fa fa-angle-double-right"></i> Periodo Escolar </a></li>
-                                <li><a href="pages/forms/form_piso.php"><i class="fa fa-angle-double-right"></i> Piso </a></li>
-                                <li><a href="pages/forms/form_docente.php"><i class="fa fa-angle-double-right"></i> Docente </a></li>
-                                <li><a href="pages/forms/form_usuario.php"><i class="fa fa-angle-double-right"></i> Usuario </a></li>
-                            </ul>
-                        </li>
-
+<!-- Menu lateral izquierdo -->                    
+<?php 
+    include("pages/menu.php");
+?>
 <!----------------------
 
                         <li class="treeview">
