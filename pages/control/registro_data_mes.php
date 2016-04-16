4726f4b60773fb712dd3dd9d6d2562bcb1004b53
Rioyi <rioyi_kaji@hotmail.co.jp>
2016-04-15 21:38:30 -0430

    Testing exitos registro Asistencia

--- pages/control/registro_data_mes.php
+++ pages/control/registro_data_mes.php
@@ -1905,25 +1905,14 @@ $a_s1_d1_p5_v = $_POST['a_s1_d1_p5_v'];
 $a_s1_d1_gi_v = $_POST['a_s1_d1_gi_v'];
 $a_s1_d1_gii_v = $_POST['a_s1_d1_gii_v'];
 
-echo "a_s1_d1_v";
-echo "a_s1_d1_p1_v";
-echo "a_s1_d1_p2_v";
-echo "a_s1_d1_p3_v";
-echo "a_s1_d1_p4_v";
-echo "a_s1_d1_p5_v";
-echo "a_s1_d1_gi_v";
-echo "a_s1_d1_gii_v";
-
-
-
-
-
-
-
-
-
-
-
+echo "$a_s1_d1_v";
+echo "$a_s1_d1_p1_v";
+echo "$a_s1_d1_p2_v";
+echo "$a_s1_d1_p3_v";
+echo "$a_s1_d1_p4_v";
+echo "$a_s1_d1_p5_v";
+echo "$a_s1_d1_gi_v";
+echo "$a_s1_d1_gii_v";
 
 #semana 1 dia 2
 
@@ -2383,7 +2372,7 @@ $a_s4_d5_gii_h = $_POST['a_s4_d5_gii_h'];
 
 
 
-/*
+
 
 mysql_query ("INSERT INTO total_asistencia (id_sexo,id_periodo_escolar,id_mes,dia,p1,p2,p3,p4,p5,gi,gii) values ('1','$periodo_e','$mes','$a_s1_d1_v','$a_s1_d1_p1_v','$a_s1_d1_p2_v','$a_s1_d1_p3_v','$a_s1_d1_p4_v','$a_s1_d1_p5_v','$a_s1_d1_gi_v','$a_s1_d1_gii_v'),
 # Semana 1 dia 2 Varones 
@@ -2429,48 +2418,49 @@ mysql_query ("INSERT INTO total_asistencia (id_sexo,id_periodo_escolar,id_mes,di
 	('1','$periodo_e','$mes','$a_s4_d5_v','$a_s4_d5_p1_v','$a_s4_d5_p2_v','$a_s4_d5_p3_v','$a_s4_d5_p4_v','$a_s4_d5_p5_v','$a_s4_d5_gi_v','$a_s4_d5_gii_v'),
 
 #--------- HEMBRAS --------------
-
+# Semana 1 dia 1 HEMBRAS 
+	('2','$periodo_e','$mes','$a_s1_d1_h','$a_s1_d1_p1_h','$a_s1_d1_p2_h','$a_s1_d1_p3_h','$a_s1_d1_p4_h','$a_s1_d1_p5_h','$a_s1_d1_gi_h','$a_s1_d1_gii_h'),
 # Semana 1 dia 2 HEMBRAS 
-	('1','$periodo_e','$mes','$a_s1_d2_h','$a_s1_d2_p1_h','$a_s1_d2_p2_h','$a_s1_d2_p3_h','$a_s1_d2_p4_h','$a_s1_d2_p5_h','$a_s1_d2_gi_h','$a_s1_d2_gii_h'),
+	('2','$periodo_e','$mes','$a_s1_d2_h','$a_s1_d2_p1_h','$a_s1_d2_p2_h','$a_s1_d2_p3_h','$a_s1_d2_p4_h','$a_s1_d2_p5_h','$a_s1_d2_gi_h','$a_s1_d2_gii_h'),
 # Semana 1 dia 3 HEMBRAS 
-	('1','$periodo_e','$mes','$a_s1_d3_h','$a_s1_d3_p1_h','$a_s1_d3_p2_h','$a_s1_d3_p3_h','$a_s1_d3_p4_h','$a_s1_d3_p5_h','$a_s1_d3_gi_h','$a_s1_d3_gii_h'),
+	('2','$periodo_e','$mes','$a_s1_d3_h','$a_s1_d3_p1_h','$a_s1_d3_p2_h','$a_s1_d3_p3_h','$a_s1_d3_p4_h','$a_s1_d3_p5_h','$a_s1_d3_gi_h','$a_s1_d3_gii_h'),
 # Semana 1 dia 4 HEMBRAS 
-	('1','$periodo_e','$mes','$a_s1_d4_h','$a_s1_d4_p1_h','$a_s1_d4_p2_h','$a_s1_d4_p3_h','$a_s1_d4_p4_h','$a_s1_d4_p5_h','$a_s1_d4_gi_h','$a_s1_d4_gii_h'),
+	('2','$periodo_e','$mes','$a_s1_d4_h','$a_s1_d4_p1_h','$a_s1_d4_p2_h','$a_s1_d4_p3_h','$a_s1_d4_p4_h','$a_s1_d4_p5_h','$a_s1_d4_gi_h','$a_s1_d4_gii_h'),
 # Semana 1 dia 5 HEMBRAS 
-	('1','$periodo_e','$mes','$a_s1_d5_h','$a_s1_d5_p1_h','$a_s1_d5_p2_h','$a_s1_d5_p3_h','$a_s1_d5_p4_h','$a_s1_d5_p5_h','$a_s1_d5_gi_h','$a_s1_d5_gii_h'),
+	('2','$periodo_e','$mes','$a_s1_d5_h','$a_s1_d5_p1_h','$a_s1_d5_p2_h','$a_s1_d5_p3_h','$a_s1_d5_p4_h','$a_s1_d5_p5_h','$a_s1_d5_gi_h','$a_s1_d5_gii_h'),
 
 # Semana 2 dia 1 HEMBRAS 
-	('1','$periodo_e','$mes','$a_s2_d1_h','$a_s2_d1_p1_h','$a_s2_d1_p2_h','$a_s2_d1_p3_h','$a_s2_d1_p4_h','$a_s2_d1_p5_h','$a_s2_d1_gi_h','$a_s2_d1_gii_h'),
+	('2','$periodo_e','$mes','$a_s2_d1_h','$a_s2_d1_p1_h','$a_s2_d1_p2_h','$a_s2_d1_p3_h','$a_s2_d1_p4_h','$a_s2_d1_p5_h','$a_s2_d1_gi_h','$a_s2_d1_gii_h'),
 # Semana 2 dia 2 HEMBRAS 
-	('1','$periodo_e','$mes','$a_s2_d2_h','$a_s2_d2_p1_h','$a_s2_d2_p2_h','$a_s2_d2_p3_h','$a_s2_d2_p4_h','$a_s2_d2_p5_h','$a_s2_d2_gi_h','$a_s2_d2_gii_h'),
+	('2','$periodo_e','$mes','$a_s2_d2_h','$a_s2_d2_p1_h','$a_s2_d2_p2_h','$a_s2_d2_p3_h','$a_s2_d2_p4_h','$a_s2_d2_p5_h','$a_s2_d2_gi_h','$a_s2_d2_gii_h'),
 # Semana 2 dia 3 HEMBRAS 
-	('1','$periodo_e','$mes','$a_s2_d3_h','$a_s2_d3_p1_h','$a_s2_d3_p2_h','$a_s2_d3_p3_h','$a_s2_d3_p4_h','$a_s2_d3_p5_h','$a_s2_d3_gi_h','$a_s2_d3_gii_h'),
+	('2','$periodo_e','$mes','$a_s2_d3_h','$a_s2_d3_p1_h','$a_s2_d3_p2_h','$a_s2_d3_p3_h','$a_s2_d3_p4_h','$a_s2_d3_p5_h','$a_s2_d3_gi_h','$a_s2_d3_gii_h'),
 # Semana 2 dia 4 HEMBRAS 
-	('1','$periodo_e','$mes','$a_s2_d4_h','$a_s2_d4_p1_h','$a_s2_d4_p2_h','$a_s2_d4_p3_h','$a_s2_d4_p4_h','$a_s2_d4_p5_h','$a_s2_d4_gi_h','$a_s2_d4_gii_h'),
+	('2','$periodo_e','$mes','$a_s2_d4_h','$a_s2_d4_p1_h','$a_s2_d4_p2_h','$a_s2_d4_p3_h','$a_s2_d4_p4_h','$a_s2_d4_p5_h','$a_s2_d4_gi_h','$a_s2_d4_gii_h'),
 # Semana 2 dia 5 HEMBRAS 
-	('1','$periodo_e','$mes','$a_s2_d5_h','$a_s2_d5_p1_h','$a_s2_d5_p2_h','$a_s2_d5_p3_h','$a_s2_d5_p4_h','$a_s2_d5_p5_h','$a_s2_d5_gi_h','$a_s2_d5_gii_h'),
+	('2','$periodo_e','$mes','$a_s2_d5_h','$a_s2_d5_p1_h','$a_s2_d5_p2_h','$a_s2_d5_p3_h','$a_s2_d5_p4_h','$a_s2_d5_p5_h','$a_s2_d5_gi_h','$a_s2_d5_gii_h'),
 
 # Semana 3 dia 1 HEMBRAS 
-	('1','$periodo_e','$mes','$a_s3_d1_h','$a_s3_d1_p1_h','$a_s3_d1_p2_h','$a_s3_d1_p3_h','$a_s3_d1_p4_h','$a_s3_d1_p5_h','$a_s3_d1_gi_h','$a_s3_d1_gii_h'),
+	('2','$periodo_e','$mes','$a_s3_d1_h','$a_s3_d1_p1_h','$a_s3_d1_p2_h','$a_s3_d1_p3_h','$a_s3_d1_p4_h','$a_s3_d1_p5_h','$a_s3_d1_gi_h','$a_s3_d1_gii_h'),
 # Semana 3 dia 2 HEMBRAS 
-	('1','$periodo_e','$mes','$a_s3_d2_h','$a_s3_d2_p1_h','$a_s3_d2_p2_h','$a_s3_d2_p3_h','$a_s3_d2_p4_h','$a_s3_d2_p5_h','$a_s3_d2_gi_h','$a_s3_d2_gii_h'),
+	('2','$periodo_e','$mes','$a_s3_d2_h','$a_s3_d2_p1_h','$a_s3_d2_p2_h','$a_s3_d2_p3_h','$a_s3_d2_p4_h','$a_s3_d2_p5_h','$a_s3_d2_gi_h','$a_s3_d2_gii_h'),
 # Semana 3 dia 3 HEMBRAS 
-	('1','$periodo_e','$mes','$a_s3_d3_h','$a_s3_d3_p1_h','$a_s3_d3_p2_h','$a_s3_d3_p3_h','$a_s3_d3_p4_h','$a_s3_d3_p5_h','$a_s3_d3_gi_h','$a_s3_d3_gii_h'),
+	('2','$periodo_e','$mes','$a_s3_d3_h','$a_s3_d3_p1_h','$a_s3_d3_p2_h','$a_s3_d3_p3_h','$a_s3_d3_p4_h','$a_s3_d3_p5_h','$a_s3_d3_gi_h','$a_s3_d3_gii_h'),
 # Semana 3 dia 4 HEMBRAS 
-	('1','$periodo_e','$mes','$a_s3_d4_h','$a_s3_d4_p1_h','$a_s3_d4_p2_h','$a_s3_d4_p3_h','$a_s3_d4_p4_h','$a_s3_d4_p5_h','$a_s3_d4_gi_h','$a_s3_d4_gii_h'),
+	('2','$periodo_e','$mes','$a_s3_d4_h','$a_s3_d4_p1_h','$a_s3_d4_p2_h','$a_s3_d4_p3_h','$a_s3_d4_p4_h','$a_s3_d4_p5_h','$a_s3_d4_gi_h','$a_s3_d4_gii_h'),
 # Semana 3 dia 5 HEMBRAS 
-	('1','$periodo_e','$mes','$a_s3_d5_h','$a_s3_d5_p1_h','$a_s3_d5_p2_h','$a_s3_d5_p3_h','$a_s3_d5_p4_h','$a_s3_d5_p5_h','$a_s3_d5_gi_h','$a_s3_d5_gii_h'),
+	('2','$periodo_e','$mes','$a_s3_d5_h','$a_s3_d5_p1_h','$a_s3_d5_p2_h','$a_s3_d5_p3_h','$a_s3_d5_p4_h','$a_s3_d5_p5_h','$a_s3_d5_gi_h','$a_s3_d5_gii_h'),
 
 # Semana 4 dia 1 HEMBRAS 
-	('1','$periodo_e','$mes','$a_s4_d1_h','$a_s4_d1_p1_h','$a_s4_d1_p2_h','$a_s4_d1_p3_h','$a_s4_d1_p4_h','$a_s4_d1_p5_h','$a_s4_d1_gi_h','$a_s4_d1_gii_h'),
+	('2','$periodo_e','$mes','$a_s4_d1_h','$a_s4_d1_p1_h','$a_s4_d1_p2_h','$a_s4_d1_p3_h','$a_s4_d1_p4_h','$a_s4_d1_p5_h','$a_s4_d1_gi_h','$a_s4_d1_gii_h'),
 # Semana 4 dia 2 HEMBRAS 
-	('1','$periodo_e','$mes','$a_s4_d2_h','$a_s4_d2_p1_h','$a_s4_d2_p2_h','$a_s4_d2_p3_h','$a_s4_d2_p4_h','$a_s4_d2_p5_h','$a_s4_d2_gi_h','$a_s4_d2_gii_h'),
+	('2','$periodo_e','$mes','$a_s4_d2_h','$a_s4_d2_p1_h','$a_s4_d2_p2_h','$a_s4_d2_p3_h','$a_s4_d2_p4_h','$a_s4_d2_p5_h','$a_s4_d2_gi_h','$a_s4_d2_gii_h'),
 # Semana 4 dia 3 HEMBRAS 
-	('1','$periodo_e','$mes','$a_s4_d3_h','$a_s4_d3_p1_h','$a_s4_d3_p2_h','$a_s4_d3_p3_h','$a_s4_d3_p4_h','$a_s4_d3_p5_h','$a_s4_d3_gi_h','$a_s4_d3_gii_h'),
+	('2','$periodo_e','$mes','$a_s4_d3_h','$a_s4_d3_p1_h','$a_s4_d3_p2_h','$a_s4_d3_p3_h','$a_s4_d3_p4_h','$a_s4_d3_p5_h','$a_s4_d3_gi_h','$a_s4_d3_gii_h'),
 # Semana 4 dia 4 HEMBRAS 
-	('1','$periodo_e','$mes','$a_s4_d4_h','$a_s4_d4_p1_h','$a_s4_d4_p2_h','$a_s4_d4_p3_h','$a_s4_d4_p4_h','$a_s4_d4_p5_h','$a_s4_d4_gi_h','$a_s4_d4_gii_h'),
+	('2','$periodo_e','$mes','$a_s4_d4_h','$a_s4_d4_p1_h','$a_s4_d4_p2_h','$a_s4_d4_p3_h','$a_s4_d4_p4_h','$a_s4_d4_p5_h','$a_s4_d4_gi_h','$a_s4_d4_gii_h'),
 # Semana 4 dia 5 HEMBRAS 
-	('1','$periodo_e','$mes','$a_s4_d5_h','$a_s4_d5_p1_h','$a_s4_d5_p2_h','$a_s4_d5_p3_h','$a_s4_d5_p4_h','$a_s4_d5_p5_h','$a_s4_d5_gi_h','$a_s4_d5_gii_h')
+	('2','$periodo_e','$mes','$a_s4_d5_h','$a_s4_d5_p1_h','$a_s4_d5_p2_h','$a_s4_d5_p3_h','$a_s4_d5_p4_h','$a_s4_d5_p5_h','$a_s4_d5_gi_h','$a_s4_d5_gii_h')
 ")
 
 or die (mysql_error());
