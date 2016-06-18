113e27174e713f89452d893a772a83d5de5b3fac
Rioyi <rioyi_kaji@hotmail.co.jp>
2016-06-18 01:47:21 -0400

    Se valido el formulario de registrar periodo escolar

--- pages/forms/form_periodo.php
+++ pages/forms/form_periodo.php
@@ -20,13 +20,11 @@
         <link href="../../css/timepicker/bootstrap-timepicker.min.css" rel="stylesheet"/>
         <!-- Theme style -->
         <link href="../../css/AdminLTE.css" rel="stylesheet" type="text/css" />
+        
 
-        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
-          <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-          <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-        <![endif]-->
+
+
+
     </head>
      <body class="skin-blue">
         <!-- header logo: style can be found in header.less -->
@@ -141,8 +139,8 @@
                                 <div class="box-header">
                                     <h3 class="box-title">Registrar Periodo Escolar</h3>
                                 </div>
-                            <!-- inicio de form -->
-                            <form action="../control/registro_periodo.php" method="post" >
+                            <!-- inicio de form action="../control/registro_periodo.php"-->
+                            <form action="../control/registro_periodo.php" method="post" id="formulario" >
 
                                 <div class="box-body">
                                     <!-- Nombre -->
@@ -152,8 +150,9 @@
                                             <div class="input-group-addon">
                                                 <i class="fa fa-calendar"></i>
                                             </div>
-                                            <input type="text" name="inicio" class="form-control" required="es requerido" data-inputmask="'alias': 'dd/mm/yyyy'" data-mask/>
-                                        </div><!-- /.input group -->
+                                            <input type="text" id="inicio" name="inicio" class="form-control" />
+                                        </div>
+                                        <!-- /.input group -->
                                     </div><!-- /.form group -->
 
                                     <div class="form-group">
@@ -162,13 +161,13 @@
                                             <div class="input-group-addon">
                                                 <i class="fa fa-calendar"></i>
                                             </div>
-                                            <input type="text" name="final" class="form-control" data-inputmask="'alias': 'dd/mm/yyyy'" data-mask/>
+                                            <input type="text" name="final" class="form-control" id="fin" />
                                         </div><!-- /.input group -->
                                     </div><!-- /.form group -->
 
                                     
 
-                                    <button type="submit" class="btn btn-primary">REGISTRAR</button>
+                                    <button type="submit" id="btn" class="btn btn-primary">REGISTRAR</button>
 
                                    
 
@@ -287,11 +286,41 @@ require ("../control/conexion_bd.php");
                 </section><!-- /.content -->
             </aside><!-- /.right-side -->
         </div><!-- ./wrapper -->
+        <script type="text/javascript" src="../../js/valida/lib/jquery-1.11.1.js"></script>
+		<script src="../../js/valida/lib/jquery.js"></script>
+		<script src="../../js/valida/lib/jquery.mockjax.js"></script>
+		<script src="../../js/valida/lib/jquery.form.js"></script>
+		<script src="../../js/valida/dist/jquery.validate.js"></script>
+		<script src="../../js/valida/lib/jquery-1.11.1.js"></script>
+		<script src="../../js/valida/dist/jquery.validate.js"></script>
+       <script>
+		$(function(){
+			$("#btn").on("click", function(){
+				$("#formulario").validate 
+					({
+					rules:
+						{
+						inicio:{required: true,digits: true, minlength: 4, maxlength: 4},
+						final:{required: true, digits: true, minlength: 4, maxlength: 4}
+						},
+						messages:
+						{
+							inicio:{required: '<font color="red"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span> Este campo es requerido</font>', minlength:'<font color="red"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span> El mínimo de caracteres son 4</font>', maxlength:'<font color="red"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span> El maximo de caracteres son 4</font>',digits:'<font color="red"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span> Solo Números</font>'},
+							final:{required: '<font color="red"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span> Este campo es requerido</font>', minlength:'<font color="red"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span> El mínimo de caracteres son 4</font>', maxlength:'<font color="red"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span> El maximo de caracteres son 4</font>',digits:'<font color="red"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span> Solo Números</font>'}
+						}
+					
+				});
+			});
+		});
 
+	</script>
 
         <!-- jQuery 2.0.2 
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script> -->
-        <script src="../../js/jquery-2.2.0.min.js" type="text/javascript"></script>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script> 
+		<script src="../../js/jquery-2.2.0.min.js" type="text/javascript"></script>
+	
+        -->
+        
         <!-- Bootstrap -->
         <script src="../../js/bootstrap.min.js" type="text/javascript"></script>
         <!-- InputMask -->
@@ -308,65 +337,7 @@ require ("../control/conexion_bd.php");
         <script src="../../js/AdminLTE/app.js" type="text/javascript"></script>
 
         <!-- Page script -->
-        <script type="text/javascript">
-            $(function() {
-                //Datemask dd/mm/yyyy
-                $("#datemask").inputmask("dd/mm/yyyy", {"placeholder": "dd/mm/yyyy"});
-                //Datemask2 mm/dd/yyyy
-                $("#datemask2").inputmask("mm/dd/yyyy", {"placeholder": "mm/dd/yyyy"});
-                //Money Euro
-                $("[data-mask]").inputmask();
-
-                //Date range picker
-                $('#reservation').daterangepicker();
-                //Date range picker with time picker
-                $('#reservationtime').daterangepicker({timePicker: true, timePickerIncrement: 30, format: 'MM/DD/YYYY h:mm A'});
-                //Date range as a button
-                $('#daterange-btn').daterangepicker(
-                        {
-                            ranges: {
-                                'Today': [moment(), moment()],
-                                'Yesterday': [moment().subtract('days', 1), moment().subtract('days', 1)],
-                                'Last 7 Days': [moment().subtract('days', 6), moment()],
-                                'Last 30 Days': [moment().subtract('days', 29), moment()],
-                                'This Month': [moment().startOf('month'), moment().endOf('month')],
-                                'Last Month': [moment().subtract('month', 1).startOf('month'), moment().subtract('month', 1).endOf('month')]
-                            },
-                            startDate: moment().subtract('days', 29),
-                            endDate: moment()
-                        },
-                function(start, end) {
-                    $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
-                }
-                );
-
-                //iCheck for checkbox and radio inputs
-                $('input[type="checkbox"].minimal, input[type="radio"].minimal').iCheck({
-                    checkboxClass: 'icheckbox_minimal',
-                    radioClass: 'iradio_minimal'
-                });
-                //Red color scheme for iCheck
-                $('input[type="checkbox"].minimal-red, input[type="radio"].minimal-red').iCheck({
-                    checkboxClass: 'icheckbox_minimal-red',
-                    radioClass: 'iradio_minimal-red'
-                });
-                //Flat red color scheme for iCheck
-                $('input[type="checkbox"].flat-red, input[type="radio"].flat-red').iCheck({
-                    checkboxClass: 'icheckbox_flat-red',
-                    radioClass: 'iradio_flat-red'
-                });
-
-                //Colorpicker
-                $(".my-colorpicker1").colorpicker();
-                //color picker with addon
-                $(".my-colorpicker2").colorpicker();
-
-                //Timepicker
-                $(".timepicker").timepicker({
-                    showInputs: false
-                });
-            });
-        </script>
+       
 
     </body>
 </html>
