87bb1440a6b1e9907b51049e411005706b997f28
Rioyi <wilbert.angarita@gmail.com>
2016-07-05 13:53:51 -0400

    Se agrego en registro de de pariodo escolar comprobacion de registro duplicados

--- pages/control/registro_periodo.php
+++ pages/control/registro_periodo.php
@@ -8,19 +8,28 @@ require("../control/conexion_bd.php");
 
 #consulta para corroborar no dublicidad de ci_escolar
 
+$consulta = mysql_num_rows(mysql_query("SELECT inicio FROM periodo_escolar WHERE inicio='$inicio'"));
+if(!$consulta==0){
+  header('location: ../emergente/duplicado.php');
+}else{
+    echo 'Hay un total de '.$consulta.' usuarios';
+
+
+
+
 			#REGISTRO DOCENTE
 			mysql_query("INSERT INTO periodo_escolar (inicio,fin) VALUES ('$inicio','$final')") or die (mysql_error());
-			
-			
-			
+
+
+
 			mysql_close($link);
 			header('location: ../emergente/registro_periodo_exitoso.php');
-			echo "Se a Registrado el periodo_escolar";
-			echo "<html><br><html>";
-			echo "<html><a href='../../index.html'>inicio</a><html>";
-		
+			#echo "Se a Registrado el periodo_escolar";
+			#echo "<html><br><html>";
+			#echo "<html><a href='../../index.html'>inicio</a><html>";
 
 
+}
 
 
 
